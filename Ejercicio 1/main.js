075491e05203c5014f50cd3de77a427761b6fff1
IlimaG <ivanilimag@gmail.com>
2022-10-26 20:55:37 +0200

    Ejercicio 2

---
+++ Ejercicio 1/main.js
@@ -0,0 +1,30 @@
+const user = {
+    name: "Cristina",
+    lasName: "La Veneno",
+    id: 69,
+}
+
+const book1 = {
+    title: "Digo! Ni puta Ni santa",
+    author: "Valeria Vegas",
+    isbn: "123123123",
+    category: "Obligatory literature"
+}
+
+const book2 = {
+    title: "Veneno. De Adra a las estrellas",
+    author: "Valeria Vegas",
+    isbn: "456456456",
+    category: "Optional literature"
+}
+
+// ITERATION 1
+
+user.books = [book1, book2]
+console.log(user);
+
+
+
+
+
+
